838d56376a008d8713ee7769969b76d5ddd499fb
Specer Krug <spncrkrug@gmail.com>
2020-03-12 18:10:00 -0400

    Add project files.

---
+++ MunitionStockAndSupply/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MunitionStockAndSupply.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
